bc5f5da1984581064d5cd29208cf72ea5a216028
GMKrishna-5 <132436497+GMKrishna-5@users.noreply.github.com>
2024-02-14 13:27:35 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "reddit-devops" # Replace with your actual S3 bucket name
-    key    = "EKS/terraform.tfstate"
-    region = "ap-southeast-2"
+    bucket         = "reddit-devops"
+    key            = "path/to/your/terraform.tfstate"
+    region         = "ap-southeast-2"
   }
 }
